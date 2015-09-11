3afad53218773f86a9e6556d37c758a7f77aac23
user105 <user105@192.168.23.119>
2015-09-11 17:12:40 +0200

    tijdelijk opload einde van de dag

--- jjaris/src/entities/VerlofAanvraag.java
+++ jjaris/src/entities/VerlofAanvraag.java
@@ -1,12 +1,17 @@
 package entities;
 
 import java.io.Serializable;
-import java.time.LocalDate;
+import java.time.DayOfWeek;
+import java.time.GregorianCalendar;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class VerlofAanvraag implements Serializable{
@@ -15,41 +20,126 @@ public class VerlofAanvraag implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
-	private LocalDate startdatum;
-	private LocalDate einddatum;
-	private LocalDate aanvraagdatum;
-	private LocalDate reactiedatum;
+	@Temporal(TemporalType.DATE)
+	private GregorianCalendar startdatum;
+	@Temporal(TemporalType.DATE)
+	private GregorianCalendar einddatum;
+	@Temporal(TemporalType.DATE)
+	private GregorianCalendar aanvraagdatum;
+	@Temporal(TemporalType.DATE)
+	private GregorianCalendar reactiedatum;
 	private Toestand toestand;
 	private String reden;
 	private Werknemer werknemer;
+	
+	
+	
+	/**
+	 * Lege Constructor
+	 * 
+	 */
+	public VerlofAanvraag() {
+	}
+	
+	/**
+	 * Constructor met startdatum einddatum 
+	 * toestand op INGEDIEND
+	 * aanvraagdatum op now
+	 */
+	public VerlofAanvraag(GregorianCalendar startDatum, GregorianCalendar eindDatum);
+	
+	//TODO
+	/**
+	 * enkel weekdagen 
+	 * 
+	 * NOG GEEN REKENING GEHOUDEN MET FEESTDAGEN OF 
+	 * 
+	 * @return aantal dagen die deze verlofaanvraag bevat 
+	 */
+	public int getOpTeNemenDagen(){
+		
+		
+		//TODO
+		return 0;	
+	}
+	/**
+	 * Methode om start en einddatum in een keer te setten met geîntegreerde check
+	 * 
+	 * @param startDatum
+	 * @param eindDatum
+	 */
+	public void setPeriode(GregorianCalendar startDatum, GregorianCalendar eindDatum){
+		//TODO
+	}
+	
+	/**
+	 * hulpmethode
+	 * Check of een verlof kan aangevraagd worden
+	 * -einddatum na begindatum
+	 * -Startdatum voor einddatum +14 dagen
+	 * @return true als een verlof kan aangevraagd worden
+	 * 
+	 */
+	public boolean geldigVerlof(GregorianCalendar startDatum, GregorianCalendar eindDatum){
+		GregorianCalendar now = new GregorianCalendar();
+		now.add(Calendar.DAY_OF_MONTH, 14);
+		if(startDatum.before(eindDatum) && startDatum.before(now)){	
+			return true;
+		}	
+		return false;
+		
+	}
+	
+	/**
+	 * Verlofaanvraag wordt goedgekeurd door teamleader
+	 */
+	public void goedkeuren(){
+		setToestand(Toestand.GOEDGEKEURD);
+		}
+	
+	/**
+	 *Verlofaanvraag wordt afgekeurd door teamleader 
+	 */
+	public void afkeuren(String reden){
+		setToestand(Toestand.AFGEKEURD);
+		setReden(reden);
+	}
+	/**
+	 * Verlofaanvraag wordt geannuleerd 
+	 */
+	public void annuleren(VerlofAanvraag aanvraag){
+		setToestand(Toestand.GEANNULEERD);
+	}
+	
+	
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
-	public LocalDate getStartdatum() {
+	public GregorianCalendar getStartdatum() {
 		return startdatum;
 	}
-	public void setStartdatum(LocalDate startdatum) {
-		this.startdatum = startdatum;
-	}
-	public LocalDate getEinddatum() {
+//	public void setStartdatum(GregorianCalendar startdatum) {
+//		this.startdatum = startdatum;
+//	}
+	public GregorianCalendar getEinddatum() {
 		return einddatum;
 	}
-	public void setEinddatum(LocalDate einddatum) {
-		this.einddatum = einddatum;
-	}
-	public LocalDate getAanvraagdatum() {
+//	public void setEinddatum(GregorianCalendar einddatum) {
+//		this.einddatum = einddatum;
+//	}
+	public GregorianCalendar getAanvraagdatum() {
 		return aanvraagdatum;
 	}
-	public void setAanvraagdatum(LocalDate aanvraagdatum) {
+	public void setAanvraagdatum(GregorianCalendar aanvraagdatum) {
 		this.aanvraagdatum = aanvraagdatum;
 	}
-	public LocalDate getReactiedatum() {
+	public GregorianCalendar getReactiedatum() {
 		return reactiedatum;
 	}
-	public void setReactiedatum(LocalDate reactiedatum) {
+	public void setReactiedatum(GregorianCalendar reactiedatum) {
 		this.reactiedatum = reactiedatum;
 	}
 	public Toestand getToestand() {
