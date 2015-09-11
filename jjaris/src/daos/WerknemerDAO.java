b71c772e52a786427fd868df537d6e5fc5a9e05a
irisdelvaux <irisdelvaux@hotmail.com>
2015-09-11 15:58:23 +0200

    WerknemerDAO voegwerknemerToe aangepast - iris

--- jjaris/src/daos/WerknemerDAO.java
+++ jjaris/src/daos/WerknemerDAO.java
@@ -1,18 +1,29 @@
 package daos;
 
+
+import javax.persistence.EntityExistsException;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TransactionRequiredException;
 import javax.transaction.Transactional;
 
 import entities.Werknemer;
 
 public class WerknemerDAO {
-	@PersistenceContext
+@PersistenceContext(unitName = "jjaris")
 private static EntityManager em ;
 	
-	
+/**
+ * 	Voegt een werknemer toe aan de database. Via Cascade zou ook het adres en het Jaarlijks verlof moeten ingevuld worden
+ * @param werknemer:Werknemer
+ * @throws EntityExistsException als er al een werknemer met dezelfde id bestaat
+ * @throws TransactionRequiredException als er een probleem is met het opzetten van de tranacties
+ */
 	@Transactional
 	public static void voegWerknemerToe(Werknemer werknemer){
-		em.get
+		
+		em.persist(werknemer);
+		
+		
 	}
 }
