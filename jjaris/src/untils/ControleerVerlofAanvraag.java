e3e15718f8ad8d93636b2b9ece6c80d3fa9806dd
jjaris <javacature@gmail.com>
2015-09-15 15:42:34 +0200

    Temp update UtilCollectie - Jorik

--- jjaris/src/untils/ControleerVerlofAanvraag.java
+++ jjaris/src/untils/ControleerVerlofAanvraag.java
@@ -1,29 +1,37 @@
 package untils;
 
 import java.util.Calendar;
+import java.util.InputMismatchException;
 
+import daos.CollectieveSluitingDAO;
 import entities.VerlofAanvraag;
 import entities.Werknemer;
 
 public class ControleerVerlofAanvraag {
 
-	public static boolean isGeldigeVerlofPeriode(Werknemer werknemer, Calendar begindatum, Calendar einddatum) {
+	public static boolean isGeldigeVerlofPeriode(Werknemer werknemer, Calendar begindatum, Calendar einddatum) throws InputMismatchException {
+		if(begindatum.compareTo(einddatum) >= 0) {
+			throw new InputMismatchException("Startdatum ligt na einddatum");
+		}
+//		if(werknemer.getAantalBeschikBareVerlofDagen(jaartal)getCollectieveDagen(begindatum, einddatum) {
+//			return true;
+//		}
 		return false;
 	}
 	
 	public static int getAantalOpTeNemenDagen(VerlofAanvraag verlofAanvraag) {
-		return 0;
+		return getCollectieveDagen(verlofAanvraag.getStartdatum(), verlofAanvraag.getEinddatum());
 	}
 	
 	public static int getCollectieveDagen(Calendar begindatum, Calendar einddatum) {
-		return 0;
+		return CollectieveSluitingDAO.getCollectieveVerloven(begindatum, einddatum).size();
 	}
 	
 	public static int getAantalFeestdagenOpWeekdag(Calendar begindatum, Calendar einddatum) {
-		return 0;
+		return CollectieveSluitingDAO.getFeestdagen(begindatum, einddatum).size();
 	}
 	
 	public static int getAantalCollectieveDagen(int jaartal) {
-		return 0;
+		return CollectieveSluitingDAO.getAlleCollectieveVerloven(jaartal).stream().mapToInt(i -> i.getAantalDagen()).sum();
 	}
 }
