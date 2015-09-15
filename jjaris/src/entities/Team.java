88a4b7a703b5e54f5248a87b9b60eb0e56fbfbeb
stefBroeckx <user112@192.168.23.122>
2015-09-15 15:08:58 +0200

    update Team - stef

--- jjaris/src/entities/Team.java
+++ jjaris/src/entities/Team.java
@@ -11,14 +11,13 @@ import javax.persistence.*;
  *
  */
 @Entity
-
 public class Team implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int code;
 	private String naam;
 	private boolean HR;
-	@OneToMany(mappedBy="team")
+	@OneToMany(mappedBy = "team")
 	private List<Werknemer> teamleden;
 	@OneToOne
 	private Werknemer teamverantwoordelijke;
@@ -26,35 +25,40 @@ public class Team implements Serializable {
 
 	public Team() {
 		super();
-	} 
+	}
+
 	public String getNaam() {
 		return this.naam;
 	}
 
 	public void setNaam(String naam) {
 		this.naam = naam;
-	}   
+	}
+
 	public int getCode() {
 		return this.code;
 	}
 
 	public void setCode(int code) {
 		this.code = code;
-	}   
+	}
+
 	public boolean getHR() {
 		return this.HR;
 	}
 
 	public void setHR(boolean HR) {
 		this.HR = HR;
-	}   
+	}
+
 	public List<Werknemer> getTeamleden() {
 		return this.teamleden;
 	}
 
 	public void setTeamleden(List<Werknemer> teamleden) {
 		this.teamleden = teamleden;
-	}   
+	}
+
 	public Werknemer getTeamverantwoordelijke() {
 		return this.teamverantwoordelijke;
 	}
@@ -62,5 +66,38 @@ public class Team implements Serializable {
 	public void setTeamverantwoordelijke(Werknemer teamverantwoordelijke) {
 		this.teamverantwoordelijke = teamverantwoordelijke;
 	}
-   
+
+	// -- isHr?
+	public boolean isHr() {
+		return this.getHR();
+	}
+
+	// Lijst van verlofaanvragen tussen 2 datums aanvragen
+
+	// Lijst van verlofaanvragen met bepaalde status
+
+	// Lijst van verlofaanvragen
+
+	// voeg een teamlid toe
+	public void voegTeamlidToe(Werknemer teamlid) {
+		teamleden.add(teamlid);
+
+	}
+
+	// verwijder een teamlid
+	public void verwijderTeamlid(Werknemer teamlid) {
+	
+		teamleden.remove(teamlid);
+	}
+
+	// zit een bepaalde werknemer in dit team?
+	public boolean zitWerknemerInTeam(Werknemer werknemer) {
+		if (teamleden.contains(werknemer)) {
+			return true;
+
+		} else {
+			return false;
+		}
+
+	}
 }
