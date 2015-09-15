70f0eb084987fb73034d49d9fdf6add160132b0f
jjaris <javacature@gmail.com>
2015-09-15 15:14:40 +0200

    CollectieveSluitingDAO - Entities Imported - Jorik

---
+++ jjaris/src/daos/CollectieveSluitingDAO.java
@@ -0,0 +1,25 @@
+package daos;
+
+import java.util.Calendar;
+import java.util.List;
+import entities.CollectiefVerlof;
+import entities.Feestdag;
+
+public class CollectieveSluitingDAO {
+	
+	public static List<Feestdag> getFeestdagen (Calendar begindatum, Calendar einddatum){
+		return null;
+	}
+	
+	public static List<Feestdag> getAlleFeestdagen(int jaartal) {
+		return null;
+	}
+	
+	public static List<CollectiefVerlof> getCollectieveVerloven(Calendar begindatum, Calendar einddatum) {
+		return null;
+	}
+	
+	public static List<CollectiefVerlof> getAlleCollectieveVerloven (int jaartal) {
+		return null;
+	}
+}
