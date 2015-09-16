166386f71eaf926c2480330f7373c030846c2f06
irisdelvaux <irisdelvaux@hotmail.com>
2015-09-16 11:17:40 +0200

    team en werknemerDAO - iris

--- jjaris/src/daos/TeamDAO.java
+++ jjaris/src/daos/TeamDAO.java
@@ -2,82 +2,102 @@ package daos;
 
 import java.util.List;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 
+import org.jboss.weld.exceptions.IllegalArgumentException;
+
 import entities.Team;
 
-
+@ApplicationScoped
 public class TeamDAO {
-	
+
 	@PersistenceContext(unitName = "jjaris")
-	private static EntityManager em;
-	
+	private EntityManager em;
+
 	@Transactional
-	public static void voegTeamToe(Team team){
-		
-		
+	public void voegTeamToe(Team team) {
+
 		em.persist(team);
-		
-		
+
 	}
-	
+
+	/**
+	 * Verwijdert een team, dat niet null is uit de database, als het geen
+	 * werknemers bevat
+	 * 
+	 * @param team
+	 */
 	@Transactional
-	public static void verwijderTeam(Team team){
-		
-		
-		em.remove(team);
-		
+	public void verwijderTeam(Team team) {
+
+		if (team != null && team.getTeamleden().isEmpty()) {
+			em.find(Team.class, team);
+			em.remove(team);
+		} else {
+			if (team == null) {
+				throw new NullPointerException("TeamDAO.verwijderTeam kan niet worden uitgevoerd op null");
+			} else {
+				throw new IllegalArgumentException(
+						"TeamDAO.verwijderTeam kan niet worden uitgevoerd want er zijn nog werknemers gekoppeld aan dit team");
+			}
+		}
 	}
-	
+
+	/**
+	 *update een team in de database
+	 * @param team
+	 */
 	@Transactional
-	public static void updateTeam(Team team){
-		
+	public void updateTeam(Team team) {
+
 		Team tmp = em.find(Team.class, team.getCode());
 		tmp.setHR(team.getHR());
 		tmp.setNaam(team.getNaam());
 		tmp.setTeamleden(team.getTeamleden());
 		tmp.setTeamverantwoordelijke(team.getTeamverantwoordelijke());
-		
-		
-		
-		
+
 	}
-	
+
 	@Transactional
-	public static Team getTeam(int code){
-		
+	public Team getTeam(int code) {
 		Team team = em.find(Team.class, code);
-		
-		
 		return team;
-		
-		
+
+	}
+
+	
+	public List<Team> getTeams(String zoekNaam, String zoekVerantwoordelijke, int code) {
+
+		TypedQuery<Team> query = em.createQuery("SELECT c FROM team c WHERE c.naam = :%naam% AND " + "c.werknemer = :%leider% AND c.code = :code",
+				Team.class);
+		return (List<Team>) query.setParameter("naam", zoekNaam).setParameter("werknemer", zoekVerantwoordelijke).setParameter("code", code)
+				.getResultList();
+
+	}
+
+	
+	public List<Team> getTeams(String zoekNaam, String zoekVerantwoordelijke) {
+
+		TypedQuery<Team> query = em.createQuery("SELECT c FROM team c WHERE c.naam = :%naam% AND " + "c.werknemer = :%leider%", Team.class);
+		return (List<Team>) query.setParameter("naam", zoekNaam).setParameter("werknemer", zoekVerantwoordelijke).getResultList();
+
 	}
 	
-	@Transactional
-	public static List<Team> getTeams(String zoekNaam, String zoekVerantwoordelijke, int code){
+	/**
+	 * Geeft alle teams uit de database. Geeft null terug als er geen teams aanwezig zijn
+	 * @return List<Team>
+	 */
+	public List<Team> getTeams(){
+		List<Team> l = null;
 		
-		TypedQuery<Team> query = em.createQuery("SELECT c FROM team c WHERE c.naam = :%naam% AND "
-				+ "c.werknemer = :%leider% AND c.code = :code",Team.class);
-		return (List<Team>) query.setParameter("naam", zoekNaam)
-				.setParameter("werknemer", zoekVerantwoordelijke)
-				.setParameter("code", code).getResultList();
+			TypedQuery<Team> tqry = em.createQuery("SELECT t FROM Team t", Team.class);
+			l = tqry.getResultList();
 		
+		return l;
 	}
-	
-	@Transactional
-	public static List<Team> getTeams(String zoekNaam, String zoekVerantwoordelijke){
-		
-		TypedQuery<Team> query = em.createQuery("SELECT c FROM team c WHERE c.naam = :%naam% AND "
-				+ "c.werknemer = :%leider%",Team.class);
-		return (List<Team>) query.setParameter("naam", zoekNaam)
-				.setParameter("werknemer", zoekVerantwoordelijke).getResultList();
-		
-	}
-	
-	
 
 }
--- jjaris/src/daos/WerknemerDAO.java
+++ jjaris/src/daos/WerknemerDAO.java
@@ -1,71 +1,90 @@
 package daos;
 
-
-import java.util.ArrayList;
 import java.util.List;
-
-
-
-
-
-
-
-
-
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.EntityExistsException;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TransactionRequiredException;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
-
 import entities.Team;
 import entities.Werknemer;
 
 @ApplicationScoped
 public class WerknemerDAO {
-@PersistenceContext(unitName = "jjaris")
-private EntityManager em  ;
-	
-//static{
-//	EntityManagerFactory emf = Persistence.createEntityManagerFactory("jjaris");
-//	em = emf.createEntityManager();
-//	System.out.println("in static van werknemerDAO");
-//}
+	@PersistenceContext(unitName = "jjaris")
+	private EntityManager em;
 
-/**
- * 	Voegt een werknemer toe aan de database. Via Cascade zou ook het adres en het Jaarlijks verlof moeten ingevuld worden
- * @param werknemer:Werknemer
- * @throws EntityExistsException als er al een werknemer met dezelfde id bestaat
- * @throws TransactionRequiredException als er een probleem is met het opzetten van de tranacties
- */
+	
+	/**
+	 * Voegt een werknemer toe aan de database. Via Cascade zou ook het adres en
+	 * het Jaarlijks verlof moeten ingevuld worden
+	 * 
+	 * @param werknemer
+	 *            :Werknemer
+	 * @throws EntityExistsException
+	 *             als er al een werknemer met dezelfde id bestaat
+	 * @throws TransactionRequiredException
+	 *             als er een probleem is met het opzetten van de tranacties
+	 */
 	@Transactional
-	public void voegWerknemerToe(Werknemer werknemer){
-//		EntityTransaction tx = em.getTransaction();
-//		tx.begin();
+	public void voegWerknemerToe(Werknemer werknemer) {
 		Team team = werknemer.getTeam();
-		
-		if (team != null){
-			if (team.getCode() != 0){
-				em.find(Team.class, team.getCode());
-			}
+
+		if (team != null) {
+			if (team.getCode() == 0) {
 				em.persist(team);
+			}
 		}
 		em.persist(werknemer);
-			
-//			tx.commit();
-		
+
+	}
+
+	/**
+	 * Geeft een lijst terug van alle werknemers
+	 * 
+	 * @return List<Werknemer>
+	 */
+	public List<Werknemer> getAlleWerknemers() {
+
+		TypedQuery<Werknemer> tqry = em.createQuery("SELECT w FROM Werknemer w", Werknemer.class);
+		return tqry.getResultList();
+
 	}
 	
 	/**
-	 * Geeft een lijst terug van alle werknemers
+	 * Geeft een werknemer terug de opgegeven personeelsnummer
+	 * @param personeelnr
+	 * @return Werknemer
+	 */
+	public Werknemer getWerknemer(int personeelnr){
+		return em.find(Werknemer.class, personeelnr);
+	}
+	
+	/**
+	 * Geeft alle werknemers uit een bepaald team terug
+	 * @param teamCode
 	 * @return List<Werknemer>
 	 */
-	public List<Werknemer> getAlleWerknemers(){
-		List<Werknemer> werknemers = new ArrayList<Werknemer>();
-		return werknemers;
+	public List<Werknemer> getWerknemers (int teamCode){
+		TypedQuery<Werknemer> tqry = em.createQuery("SELECT w FROM Werknemer w WHERE w.team.code = :code", Werknemer.class);
+		tqry.setParameter("code", teamCode);
+		return tqry.getResultList();
+	}
+	
+	public void updateWerknemer(Werknemer werknemer){
+		Werknemer tmp = em.find(Werknemer.class, werknemer.getPersoneelsnummer());
+		tmp.setAdres(werknemer.getAdres());
+		tmp.setEmail(werknemer.getEmail());
+		tmp.setGeboortedatum(werknemer.getGeboortedatum());
 	}
 	
 	
+	
+	
+	
+	
+	
+
 }
