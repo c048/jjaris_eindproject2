f3aa4f1f7e3d7fa951919b6d212880e05f00b16b
jjaris <javacature@gmail.com>
2015-09-18 16:25:50 +0200

    DatumBuilder - Small Patch - Jorik

--- jjaris/src/utils/DatumBuilder.java
+++ jjaris/src/utils/DatumBuilder.java
@@ -41,7 +41,7 @@ public class DatumBuilder {
 
 	private void setDag(int dag) throws IllegalArgumentException {
 		Calendar tmpTime = new GregorianCalendar(getJaar(), getMaand(), 1);
-		if(dag > tmpTime.getActualMaximum(Calendar.MONTH) || dag < 1) {
+		if(dag > tmpTime.getActualMaximum(Calendar.MONTH) && dag < 1) {
 			throw new IllegalArgumentException(String.format("Dag moet tussen 1 en %s liggen!", tmpTime.getActualMaximum(Calendar.MONTH)));
 		}
 		this.dag = dag;
