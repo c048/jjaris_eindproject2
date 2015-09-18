5ea1b5fc1d2bb6d815a5c645e8a45265e795deb1
irisdelvaux <irisdelvaux@hotmail.com>
2015-09-18 11:00:50 +0200

    wijziging naam backing bean team - iris

--- jjaris/src/beans/CreateTeamBack.java
+++ jjaris/src/beans/CreateTeamBack.java
@@ -11,7 +11,7 @@ import entities.Team;
 
 @Named
 @RequestScoped
-public class ManageTeamBack implements Serializable {
+public class CreateTeamBack implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private String naam;
 	private String HR = "false";
