0d7e579cd2d947d711af53502a42d4efafda23a7
user105 <user105@192.168.23.119>
2015-09-21 10:03:04 +0200

    Wijzeging verlofaanvraag-->VerlofMedewerkerBack

--- jjaris/src/beans/VerlofMedewerkerBack.java
+++ jjaris/src/beans/VerlofMedewerkerBack.java
@@ -1,5 +1,6 @@
 package beans;
 
+import java.io.Serializable;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -15,20 +16,17 @@ import daos.VerlofAanvraagDAO;
 import entities.VerlofAanvraag;
 import entities.Werknemer;
 
-@Named("verlofMedewerkerBack")
+
+@Named("verlofaanvraag")
 @RequestScoped
-public class VerlofMedewerkerBack {
-	
+public class VerlofMedewerkerBack implements Serializable{
+	private static final long serialVersionUID = 1L;
 	@Inject
-	private LoginBack loginBack;
+	private LoginBack user;
 	@Inject
-	private VerlofAanvraagDAO verlofAanvragen;
-	private int filterStartJaar;
-	private int filterStartMaand;
-	private int filterStartDag;
-	private int filterEindJaar;
-	private int filterEindMaand;
-	private int filterEindDag;
+	private VerlofAanvraagDAO verlofaanvraagDAO;
+	private Date startdatum;
+	private Date einddatum;
 	private int startJaar;
 	private int startMaand;
 	private int startDag;
@@ -36,134 +34,136 @@ public class VerlofMedewerkerBack {
 	private int eindMaand;
 	private int eindDag;
 	
-	public int getFilterStartJaar() {
-		return filterStartJaar;
-	}
+	
+	
+	private Filter filter= new Filter();
+	private int zoekToestand ;
 
-	public void setFilterStartJaar(int filterStartJaar) {
-		this.filterStartJaar = filterStartJaar;
-	}
-
-	public int getFilterStartMaand() {
-		return filterStartMaand;
-	}
-
-	public void setFilterStartMaand(int filterStartMaand) {
-		this.filterStartMaand = filterStartMaand;
-	}
-
-	public int getFilterStartDag() {
-		return filterStartDag;
-	}
-
-	public void setFilterStartDag(int filterStartDag) {
-		this.filterStartDag = filterStartDag;
-	}
-
-	public int getFilterEindJaar() {
-		return filterEindJaar;
-	}
-
-	public void setFilterEindJaar(int filterEindJaar) {
-		this.filterEindJaar = filterEindJaar;
-	}
-
-	public int getFilterEindMaand() {
-		return filterEindMaand;
-	}
-
-	public void setFilterEindMaand(int filterEindMaand) {
-		this.filterEindMaand = filterEindMaand;
-	}
-
-	public int getFilterEindDag() {
-		return filterEindDag;
-	}
-
-	public void setFilterEindDag(int filterEindDag) {
-		this.filterEindDag = filterEindDag;
+	/**
+	 * Lijst met verlofaanvragen per werknemer gefilterd standaard lege filters
+	 * behalve geannulleerde aanvragen
+	 * @return
+	 */
+	public List<VerlofAanvraag> getAanvragen(){
+		filter.voegFilterToe("werknemer.personeelsnummer",user.getIngelogdeWerknemer().getPersoneelsnummer() );
+		if(startdatum!=null){filter.voegFilterToe("startdatum", converteerDatum(startdatum));}
+		if(einddatum!=null) {filter.voegFilterToe("einddatum", converteerDatum(einddatum));}
+		//else{System.out.println("***********************************geen filter");}
+		if(zoekToestand<0 && zoekToestand>3){filter.voegFilterToe("toestand", zoekToestand);}
+		//else{System.out.println("***********************************geen filter");}
+		return verlofaanvraagDAO.getVerlofAanvragen(filter);
 	}
 	
+	public String zoeken(){
+		setStartdatum();
+		setEinddatum();
+		getAanvragen();
+		return null;
+		
+	}
+	
+	/**
+	 * Annuleer een verlofaanvraag	
+	 */
+	public String annuleren(int id){
+		Werknemer werknemer = user.getIngelogdeWerknemer();
+		werknemer.annuleerVerlofAanvraag(id);
+		verlofaanvraagDAO.updateVerlofAanvraag(verlofaanvraagDAO.getVerlofAanvraag(id));
+		return null;				
+	}
+	/**
+	 * Verlof aanvragen
+	 */
+	public void toevoegen(){
+		Werknemer werknemer = user.getIngelogdeWerknemer();
+		setStartdatum();
+		setEinddatum();
+		VerlofAanvraag verlof = new VerlofAanvraag(converteerDatum(startdatum), converteerDatum(einddatum), werknemer);
+		verlofaanvraagDAO.voegVerlofAanvraagToe(verlof);	
+	}		
+	/**
+	 * hulpmethode datum date-->gregorian
+	 * @param tmp
+	 * @return
+	 */
+	private GregorianCalendar converteerDatum(Date tmp){
+		GregorianCalendar newdate =new GregorianCalendar();
+		newdate.setTime(tmp);
+		return newdate;		
+	}
+
+	
+	public Date getStartdatum() {
+		return startdatum;
+	}
+	/**
+	 * Zet datum aan de hand van de apparte velden met datumbuilder
+	 * @param startdatum
+	 */
+	public void setStartdatum() {
+		DatumBuilder tmp = new DatumBuilder(startDag, startMaand, startJaar);
+		this.startdatum = tmp.buildDate();
+	}
+	public Date getEinddatum() {
+		return einddatum;
+	}
+	/**
+	 * Zet datum aan de hand van de apparte veldenmet datumbuilder
+	 * @param einddatum
+	 */
+	public void setEinddatum() {
+		DatumBuilder tmp = new DatumBuilder(eindDag, eindMaand, eindJaar);
+		this.einddatum = tmp.buildDate();
+	}
+	
+	public Date converteerNaarDate(Calendar calendar){
+		return calendar.getTime() ;
+	}
+//	public getEinddatumString(){
+//		converteerDatum(einddatum).
+//	}
 	public int getStartJaar() {
 		return startJaar;
 	}
-
 	public void setStartJaar(int startJaar) {
 		this.startJaar = startJaar;
 	}
-
 	public int getStartMaand() {
 		return startMaand;
 	}
-
 	public void setStartMaand(int startMaand) {
 		this.startMaand = startMaand;
 	}
-
 	public int getStartDag() {
 		return startDag;
 	}
-
 	public void setStartDag(int startDag) {
 		this.startDag = startDag;
 	}
-
 	public int getEindJaar() {
 		return eindJaar;
 	}
-
 	public void setEindJaar(int eindJaar) {
 		this.eindJaar = eindJaar;
 	}
-
 	public int getEindMaand() {
 		return eindMaand;
 	}
-
 	public void setEindMaand(int eindMaand) {
 		this.eindMaand = eindMaand;
 	}
-
 	public int getEindDag() {
 		return eindDag;
 	}
-
 	public void setEindDag(int eindDag) {
 		this.eindDag = eindDag;
 	}
-	
-	public Date convertDate(Calendar calendar){
-		return calendar.getTime() ;
+	public int getZoekToestand() {
+		return zoekToestand;
 	}
-
-	public List<VerlofAanvraag> searchAanvragen() {
-		if(getFilterStartDag() == 0 || getFilterEindDag() == 0) {
-			return verlofAanvragen.getVerlofAanvragenWerknemer(loginBack.getIngelogdeWerknemer().getPersoneelsnummer());
-		} else {
-			Filter tmpFilter = new Filter();
-			tmpFilter.voegFilterToe("startdatum", new DatumBuilder(filterStartDag, filterStartMaand, filterStartJaar));
-			tmpFilter.voegFilterToe("einddatum", new DatumBuilder(filterEindDag, filterEindMaand, filterEindJaar));
-			tmpFilter.voegFilterToe("werknemer.personeelsnummer", loginBack.getIngelogdeWerknemer().getPersoneelsnummer());
-			return verlofAanvragen.getVerlofAanvragen(tmpFilter);
-		}
+	public void setZoekToestand(int zoekToestand) {
+		this.zoekToestand = zoekToestand;
 	}
 	
-	public String cancelAanvraag(int id) {
-		System.out.println("hier");
-		VerlofAanvraag tmpVerlof = verlofAanvragen.getVerlofAanvraag(id);
-		tmpVerlof.annuleren();
-		verlofAanvragen.updateVerlofAanvraag(tmpVerlof);
-		return null;
-	}
 	
-	public String createAanvraag() {
-		try {
-			Calendar tmpStart = (new DatumBuilder(startDag, startMaand, startJaar)).buildCalendar();
-			Calendar tmpEind = (new DatumBuilder(eindDag, eindMaand, eindJaar)).buildCalendar();
-			verlofAanvragen.voegVerlofAanvraagToe(new VerlofAanvraag((GregorianCalendar) tmpStart,(GregorianCalendar) tmpEind, loginBack.getIngelogdeWerknemer()));
-		} catch (Exception e) {
-			System.out.println("Error on create aanvraag: " + e.getMessage());
-		}
-		return null;
-	}
 }
---
+++ jjaris/src/beans/old_VerlofMedewerkerBack.java
@@ -0,0 +1,169 @@
+package beans;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import utils.DatumBuilder;
+import utils.Filter;
+import daos.VerlofAanvraagDAO;
+import entities.VerlofAanvraag;
+import entities.Werknemer;
+
+@Named("verlofMedewerkerBack")
+@RequestScoped
+public class old_VerlofMedewerkerBack {
+	
+	@Inject
+	private LoginBack loginBack;
+	@Inject
+	private VerlofAanvraagDAO verlofAanvragen;
+	private int filterStartJaar;
+	private int filterStartMaand;
+	private int filterStartDag;
+	private int filterEindJaar;
+	private int filterEindMaand;
+	private int filterEindDag;
+	private int startJaar;
+	private int startMaand;
+	private int startDag;
+	private int eindJaar;
+	private int eindMaand;
+	private int eindDag;
+	
+	public int getFilterStartJaar() {
+		return filterStartJaar;
+	}
+
+	public void setFilterStartJaar(int filterStartJaar) {
+		this.filterStartJaar = filterStartJaar;
+	}
+
+	public int getFilterStartMaand() {
+		return filterStartMaand;
+	}
+
+	public void setFilterStartMaand(int filterStartMaand) {
+		this.filterStartMaand = filterStartMaand;
+	}
+
+	public int getFilterStartDag() {
+		return filterStartDag;
+	}
+
+	public void setFilterStartDag(int filterStartDag) {
+		this.filterStartDag = filterStartDag;
+	}
+
+	public int getFilterEindJaar() {
+		return filterEindJaar;
+	}
+
+	public void setFilterEindJaar(int filterEindJaar) {
+		this.filterEindJaar = filterEindJaar;
+	}
+
+	public int getFilterEindMaand() {
+		return filterEindMaand;
+	}
+
+	public void setFilterEindMaand(int filterEindMaand) {
+		this.filterEindMaand = filterEindMaand;
+	}
+
+	public int getFilterEindDag() {
+		return filterEindDag;
+	}
+
+	public void setFilterEindDag(int filterEindDag) {
+		this.filterEindDag = filterEindDag;
+	}
+	
+	public int getStartJaar() {
+		return startJaar;
+	}
+
+	public void setStartJaar(int startJaar) {
+		this.startJaar = startJaar;
+	}
+
+	public int getStartMaand() {
+		return startMaand;
+	}
+
+	public void setStartMaand(int startMaand) {
+		this.startMaand = startMaand;
+	}
+
+	public int getStartDag() {
+		return startDag;
+	}
+
+	public void setStartDag(int startDag) {
+		this.startDag = startDag;
+	}
+
+	public int getEindJaar() {
+		return eindJaar;
+	}
+
+	public void setEindJaar(int eindJaar) {
+		this.eindJaar = eindJaar;
+	}
+
+	public int getEindMaand() {
+		return eindMaand;
+	}
+
+	public void setEindMaand(int eindMaand) {
+		this.eindMaand = eindMaand;
+	}
+
+	public int getEindDag() {
+		return eindDag;
+	}
+
+	public void setEindDag(int eindDag) {
+		this.eindDag = eindDag;
+	}
+	
+	public Date convertDate(Calendar calendar){
+		return calendar.getTime() ;
+	}
+
+	public List<VerlofAanvraag> searchAanvragen() {
+		if(getFilterStartDag() == 0 || getFilterEindDag() == 0) {
+			return verlofAanvragen.getVerlofAanvragenWerknemer(loginBack.getIngelogdeWerknemer().getPersoneelsnummer());
+		} else {
+			Filter tmpFilter = new Filter();
+			tmpFilter.voegFilterToe("startdatum", new DatumBuilder(filterStartDag, filterStartMaand, filterStartJaar));
+			tmpFilter.voegFilterToe("einddatum", new DatumBuilder(filterEindDag, filterEindMaand, filterEindJaar));
+			tmpFilter.voegFilterToe("werknemer.personeelsnummer", loginBack.getIngelogdeWerknemer().getPersoneelsnummer());
+			return verlofAanvragen.getVerlofAanvragen(tmpFilter);
+		}
+	}
+	
+	public String cancelAanvraag(int id) {
+		System.out.println("hier");
+		VerlofAanvraag tmpVerlof = verlofAanvragen.getVerlofAanvraag(id);
+		tmpVerlof.annuleren();
+		verlofAanvragen.updateVerlofAanvraag(tmpVerlof);
+		return null;
+	}
+	
+	public String createAanvraag() {
+		try {
+			Calendar tmpStart = (new DatumBuilder(startDag, startMaand, startJaar)).buildCalendar();
+			Calendar tmpEind = (new DatumBuilder(eindDag, eindMaand, eindJaar)).buildCalendar();
+			verlofAanvragen.voegVerlofAanvraagToe(new VerlofAanvraag((GregorianCalendar) tmpStart,(GregorianCalendar) tmpEind, loginBack.getIngelogdeWerknemer()));
+		} catch (Exception e) {
+			System.out.println("Error on create aanvraag: " + e.getMessage());
+		}
+		return null;
+	}
+}
