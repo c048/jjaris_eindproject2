808928bccf68906c7816e19497c5e0e4e829ed49
user105 <user105@192.168.23.119>
2015-09-22 09:13:15 +0200

    Robin

--- jjaris/src/beans/VerlofAanvraagHr.java
+++ jjaris/src/beans/VerlofAanvraagHr.java
@@ -12,7 +12,9 @@ import javax.inject.Named;
 
 import utils.DatumBuilder;
 import utils.Filter;
+import daos.TeamDAO;
 import daos.VerlofAanvraagDAO;
+import entities.Team;
 import entities.Toestand;
 import entities.VerlofAanvraag;
 import entities.Werknemer;
@@ -26,33 +28,102 @@ public class VerlofAanvraagHr implements Serializable{
 	private LoginBack user;
 	@Inject
 	private VerlofAanvraagDAO verlofaanvraagDAO;
-	private Date startdatum;
-	private Date einddatum;
+	@Inject
+	private TeamDAO teamDAO;
 	private int startJaar;
 	private int startMaand;
 	private int startDag;
 	private int eindJaar;
 	private int eindMaand;
 	private int eindDag;
-	private Toestand toestand;
-	private List<VerlofAanvraag> alleVerloven;
+	private Toestand toestand =  null;
+	private Team team;
+	private int teamID;
+	private String voorNaam;
 	
+	private String achterNaam;
 	
-	
-	
-	
-	public Date getStartdatum() {
-		return startdatum;
+	private Filter filter= new Filter();
+
+	/**
+	 * Lijst met verlofaanvragen per werknemer gefilterd standaard lege filters
+	 * behalve geannulleerde aanvragen
+	 * @return
+	 */
+	public List<VerlofAanvraag> getAanvragen(){
+		Date startdatum = buildDatum(startDag, startMaand, startJaar);
+		Date einddatum = buildDatum(eindDag, eindMaand, eindJaar);
+		filter.voegFilterToe("werknemer.personeelsnummer",user.getIngelogdeWerknemer().getPersoneelsnummer() );
+		if(startdatum!=null){filter.voegFilterToe("startdatum", converteerDatum(startdatum));}
+		if(einddatum!=null) {filter.voegFilterToe("einddatum", converteerDatum(einddatum));}
+		if(toestand !=null){filter.voegFilterToe("toestand", toestand);}
+		if(voorNaam !=null){filter.voegFilterToe("toest", voorNaam);}
+		if(achterNaam !=null){filter.voegFilterToe("toe", achterNaam);}
+		return verlofaanvraagDAO.getVerlofAanvragen(filter);
 	}
-	public void setStartdatum(Date startdatum) {
-		this.startdatum = startdatum;
+	
+	public String zoeken(){
+		getAanvragen();
+		return null;
+		
 	}
-	public Date getEinddatum() {
-		return einddatum;
+	
+	/**
+	 * Annuleer een verlofaanvraag	
+	 */
+	public String annuleren(int id){
+		Werknemer werknemer = user.getIngelogdeWerknemer();
+		werknemer.annuleerVerlofAanvraag(id);
+		verlofaanvraagDAO.updateVerlofAanvraag(verlofaanvraagDAO.getVerlofAanvraag(id));
+		return null;				
 	}
-	public void setEinddatum(Date einddatum) {
-		this.einddatum = einddatum;
+	/**
+	 * Verlof aanvragen
+	 */
+	public void toevoegen(){
+		Date startdatum = buildDatum(startDag, startMaand, startJaar);
+		Date einddatum = buildDatum(eindDag, eindMaand, eindJaar);
+		Werknemer werknemer = user.getIngelogdeWerknemer();
+		VerlofAanvraag verlof = new VerlofAanvraag(converteerDatum(startdatum), converteerDatum(einddatum), werknemer);
+		verlofaanvraagDAO.voegVerlofAanvraagToe(verlof);	
+	}		
+	/**
+	 * hulpmethode datum date-->gregorian
+	 * @param tmp
+	 * @return
+	 */
+	private GregorianCalendar converteerDatum(Date tmp){
+		GregorianCalendar newdate =new GregorianCalendar();
+		newdate.setTime(tmp);
+		return newdate;		
 	}
+
+	
+
+	/**
+	 * Zet datum aan de hand van de apparte velden met datumbuilder
+	 * @param startdatum
+	 */
+	
+	private Date buildDatum(int dag, int maand, int jaar){
+		if (dag != 0 && maand != 0 && jaar !=0){
+		DatumBuilder tmp = new DatumBuilder(dag, maand, jaar);
+		return tmp.buildDate();
+		}
+		else{
+			return null;
+		}
+	}
+	
+	/**
+	 * Zet datum aan de hand van de apparte veldenmet datumbuilder
+	 * @param einddatum
+	 */
+
+	public Date converteerNaarDate(Calendar calendar){
+		return calendar.getTime() ;
+	}
+
 	public int getStartJaar() {
 		return startJaar;
 	}
@@ -86,23 +157,54 @@ public class VerlofAanvraagHr implements Serializable{
 	public int getEindDag() {
 		return eindDag;
 	}
+	
+	public int getTeamID() {
+		return teamID;
+	}
+
+	public void setTeamID(int teamID) {
+		this.teamID = teamID;
+	}
+
 	public void setEindDag(int eindDag) {
 		this.eindDag = eindDag;
 	}
-	public Toestand getToestand() {
-		return toestand;
+
+	public String getToestand() {
+		return toestand == null ? "": toestand.toString();
 	}
-	public void setToestand(Toestand toestand) {
-		this.toestand = toestand;
+
+	public void setToestand(String toestand) {
+		if(toestand != null && !toestand.isEmpty ()) {
+			this.toestand = Toestand.valueOf(toestand);
+		}
 	}
-	public List<VerlofAanvraag> getAlleVerloven() {
-		return alleVerloven;
+
+
+	public String getVoorNaam() {
+		return voorNaam;
 	}
-	public void setAlleVerloven(List<VerlofAanvraag> alleVerloven) {
-		this.alleVerloven = alleVerloven;
+
+	public void setVoorNaam(String voorNaam) {
+		this.voorNaam = voorNaam;
 	}
-	
-	
-	
-	
+
+	public String getAchterNaam() {
+		return achterNaam;
+	}
+
+	public void setAchterNaam(String achterNaam) {
+		this.achterNaam = achterNaam;
+	}
+
+	public Team getTeam() {
+		return team;
+	}
+
+	public void setTeam(Team team) {
+		this.team = team;
+	}
+	public List<Team> teamlijst(){
+		return teamDAO.getTeams();
+	}
 }
