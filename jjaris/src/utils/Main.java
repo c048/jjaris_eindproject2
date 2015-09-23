07a03470f2fd925792bf5644f6a2b71361e9de59
user105 <user105@192.168.23.119>
2015-09-23 11:10:20 +0200

    Niewe classe SendMail met static methode send e-mail Aanpassing pom xml Javak mail toegevoegd

---
+++ jjaris/src/utils/Main.java
@@ -0,0 +1,10 @@
+package utils;
+
+// vooraf: javax.mail.jar toegevoegd aan build path
+// bron: http://www.tutorialspoint.com/java/java_sending_email.htm
+
+
+
+public class Main {
+	public static void main(String[] args) {
+
--- jjaris/src/utils/SendMail.java
+++ jjaris/src/utils/SendMail.java
@@ -1,57 +1,46 @@
 package utils;
+
 import java.util.Properties;
-import javax.mail.Authenticator;
+
 import javax.mail.Message;
-import javax.mail.Message.RecipientType;
 import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-public class SendMail {
-    public void sendMail(String m_from,String m_to,String m_subject,String m_body){
-      try {
-          Session m_Session;
-          Message m_simpleMessage;
-          InternetAddress m_fromAddress;
-          InternetAddress m_toAddress;
-          Properties m_properties;
 
-          m_properties     = new Properties();
-          m_properties.put("mail.smtp.host", "smtp.gmail.com"); 
-          m_properties.put("mail.smtp.socketFactory.port", "465");
-          m_properties.put("mail.smtp.socketFactory.class",
-                                     "javax.net.ssl.SSLSocketFactory");
-          m_properties.put("mail.smtp.auth", "true");
-          m_properties.put("mail.smtp.port", "465");
 
-          m_Session=Session.getDefaultInstance(m_properties,new Authenticator() {
-               protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication("xxxxx","yyyyy"); // username and the password
-               }
-          });
+public final class SendMail {
+	private SendMail() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public static void SendEmail(String to,String subject, String boddy) {
+		Properties props = System.getProperties();
+		props.setProperty("mail.smtp.host", "localhost");
+		Session session = Session.getDefaultInstance(System.getProperties());
+		try {
+			// Create a default MimeMessage object.
+			MimeMessage message = new MimeMessage(session);
+			// Set From: header field of the header.
+			message.setFrom(new InternetAddress("web@gmail.com"));
+			// Set To: header field of the header.
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+			// Set Subject: header field
+			message.setSubject("This is the Subject Line!");
+			// Now set the actual message
+			message.setText("This is the actual message");
+			// Send message
+			Transport.send(message);
+			System.out.println("Sent message successfully....");
+		} catch (MessagingException mex) {
+			mex.printStackTrace();
+		}
+	}
 
-          m_simpleMessage  =   new MimeMessage(m_Session);
-          m_fromAddress    =   new InternetAddress(m_from);
-          m_toAddress      =   new InternetAddress(m_to);
-          m_simpleMessage.setFrom(m_fromAddress);
-          m_simpleMessage.setRecipient(RecipientType.TO, m_toAddress);
-          m_simpleMessage.setSubject(m_subject);
+		
+		
+	}
 
-          m_simpleMessage.setContent(m_body, "text/html");       
-          //m_simpleMessage.setContent(m_body,"text/plain");
 
-          Transport.send(m_simpleMessage);
-        } catch (MessagingException ex) {
-            ex.printStackTrace();
-        }
-  }
-
-  public static void main(String[] args) {
-      SendMail send_mail    =   new SendMail();
-      String empName = "Antony Raj S";
-      String title ="<b>Hi !"+empName+"</b>";
-      send_mail.sendMail("anto@gmail.com", "anthony@slingmedia.com", "Please apply for leave for the following dates", title+"<br>by<br><b>HR<b>");
-    }
-}
